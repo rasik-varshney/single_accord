690d3f4c790ca151054b97ec52401df9e3cd9dba
rasik <rasikvarshney215@gmail.com>
2022-01-15 21:37:27 +0530

    done

---
+++ src/components/Data.js
@@ -0,0 +1,15 @@
+export const Data = [
+  {
+    question: "What is ReactJs?",
+    answer:
+      "React is a free and open-source front-end JavaScript library for building user interfaces based on UI components",
+  },
+  {
+    question: "What is Virtual DOM",
+    answer:"The virtual DOM (VDOM) is a programming concept where an ideal, or “virtual”, representation of a UI is kept in memory and synced with the “real” DOM by a library such as ReactDOM.",
+  },
+  {
+    question: "What do you like most Data-structure Front-end or Back-end ",
+    answer: "Data-structure",
+  },
+];
